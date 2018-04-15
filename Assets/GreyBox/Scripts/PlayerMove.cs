e6b38774c813f7ec76941634fe17f6963682287b
Christopher <s0557859@htw-berlin.de>
2018-04-15 17:01:29 +0200

    prototypical camera with graybox mockup

---
+++ Assets/GreyBox/Scripts/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveForce;
+    public Material activeMat;
+    public Material inactiveMat;
+    public Light myLight;
+    Rigidbody myRig;
+    // Use this for initialization
+    void OnEnable()
+    {
+        myLight.enabled = true;
+        GetComponent<MeshRenderer>().material = activeMat;
+    }
+
+    private void OnDisable()
+    {
+        myLight.enabled = false;
+        GetComponent<MeshRenderer>().material = inactiveMat;
+    }
+
+    void Start()
+    {
+        enabled = false;
+        myRig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        /* if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
+         {
+             myRig.AddForce(moveForce * Input.GetAxis("Horizontal") * Vector3.right);
+         }
+         if (Mathf.Abs(Input.GetAxis("Vertical")) > 0)
+         {
+             myRig.AddForce(moveForce * Input.GetAxis("Vertical") * Vector3.forward);
+         }*/
+        myRig.velocity = moveForce * (Input.GetAxis("Vertical") * Vector3.forward + Input.GetAxis("Horizontal") * Vector3.right);
+    }
+}
--- Assets/GreyBox/Scripts/RoomCam.cs
+++ Assets/GreyBox/Scripts/RoomCam.cs
@@ -14,10 +14,12 @@ public class RoomCam : MonoBehaviour
     public string roomTag;
     Transform camTrans;
     GameObject target;
+    GameObject targetPlayer;
     Vector3 desiredPos;
     Vector3 origPos;
     RaycastHit roomHit;
     float scrollDelta;
+    Vector3 toPlayerVector;
     private void Start()
     {
         origPos = currentCam.transform.position;
@@ -39,7 +41,12 @@ public class RoomCam : MonoBehaviour
             if (Physics.Raycast(currentCam.ScreenPointToRay(Input.mousePosition), out roomHit))
             {
                 target = roomHit.transform.parent.gameObject;
-
+                if (targetPlayer != null)
+                {
+                    targetPlayer.GetComponent<PlayerMove>().enabled = false;
+                }
+                targetPlayer = target.transform.Find("Player").gameObject;
+                targetPlayer.GetComponent<PlayerMove>().enabled = true;
                 if (roomHit.transform.parent.gameObject.CompareTag(roomTag))
                 {
                     desiredPos = target.transform.position + offSet.x * target.transform.forward + offSet.z * target.transform.right + offSet.y * target.transform.up;
@@ -49,17 +56,18 @@ public class RoomCam : MonoBehaviour
         scrollDelta = Input.GetAxis("Mouse ScrollWheel") / Mathf.Abs(Input.GetAxis("Mouse ScrollWheel"));
         if (Mathf.Abs(scrollDelta) > 0)
         {
-            if (scrollDelta > 0 &&
+            /*if (scrollDelta > 0 &&
                  target != null &&
                 desiredPos != target.transform.position + offSet.x * target.transform.forward + offSet.z * target.transform.right + offSet.y * target.transform.up)
             {
                 desiredPos = currentCam.transform.position + ( target.transform.position + offSet.x * target.transform.forward + offSet.z * target.transform.right + offSet.y * target.transform.up - origPos) * scrollSpeed;
-            }
+            }*/
             if (scrollDelta < 0 &&
                  target != null &&
-                 desiredPos != origPos)
+                Vector3.Dot(origPos - currentCam.transform.position , origPos + currentCam.transform.forward) > 0)
             {
-                desiredPos = currentCam.transform.position + (origPos - target.transform.position + offSet.x * target.transform.forward + offSet.z * target.transform.right + offSet.y * target.transform.up ) * scrollSpeed;
+                targetPlayer.GetComponent<PlayerMove>().enabled = false;
+                desiredPos = currentCam.transform.position + (origPos - target.transform.position + offSet.x * target.transform.forward + offSet.z * target.transform.right + offSet.y * target.transform.up) * scrollSpeed;
             }
         }
     }
