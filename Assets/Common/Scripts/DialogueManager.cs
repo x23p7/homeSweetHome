35dbf9f12ccf85bb2d098f3edc898225b7b064a5
Chris <umbral@web.de>
2018-05-03 17:28:07 +0200

    rough dialogue system

---
+++ Assets/Common/Scripts/DialogueManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+    public static DialogueManager instance;
+    private Queue<string> sentences;
+    DialogueTrigger currentDialogueTrigger;
+    public float letterDelay;
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator panelAnim;
+    public Animator nameAnim;
+    public Animator dialogueAnim;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        sentences = new Queue<string>();
+	}
+
+    public void StartDialogue(DialogueTrigger dialogueTrigger,Dialogue dialogue)
+    {
+        panelAnim.SetBool("panelActive", true);
+        nameAnim.SetBool("textActive", true);
+        dialogueAnim.SetBool("dialogueActive", true);
+        currentDialogueTrigger = dialogueTrigger;
+        currentDialogueTrigger.dialogueActive = true;
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(letterDelay);
+        }
+    }
+
+    public void EndDialogue()
+    {
+        currentDialogueTrigger.dialogueActive = false;
+        panelAnim.SetBool("panelActive", false);
+        nameAnim.SetBool("textActive", false);
+        dialogueAnim.SetBool("dialogueActive", false);
+        Debug.Log("Ending Conv");
+    }
+}
---
+++ Assets/Common/Scripts/DialogueTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+    public bool triggerOnButtonPress;
+    [HideInInspector]
+    public bool dialogueActive;
+    public Dialogue dialogue;
+
+    MeshRenderer myMesh;
+    // Use this for initialization
+    private void Awake()
+    {
+        myMesh = GetComponent<MeshRenderer>();
+        myMesh.enabled = false;
+    }
+    public void TriggerDialogue()
+    {
+        DialogueManager.instance.StartDialogue(this,dialogue);
+    }
+
+    private void OnTriggerStay (Collider other)
+    {
+        Debug.Log("collition with" + other.gameObject.tag);
+
+        if (other.gameObject.CompareTag("Player") && triggerOnButtonPress && InputManager.instance.jumpInputDown)
+        {
+            if (!dialogueActive) { 
+            TriggerDialogue();
+            }
+            else
+            {
+                DialogueManager.instance.DisplayNextSentence();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        myMesh.enabled = false;
+        if (dialogueActive) { 
+        DialogueManager.instance.EndDialogue();
+        }
+    }
+}
