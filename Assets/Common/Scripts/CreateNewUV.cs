3f340d5b2fc2be559bdbcec338b140d0f4243d88
Christopher <s0557859@htw-berlin.de>
2018-05-27 20:30:43 +0200

    new visual prefabs, tried out some UV runtime shenanigans

---
+++ Assets/Common/Scripts/CreateNewUV.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateNewUV : MonoBehaviour {
+    public float offSetX;
+    void Awake()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector2[] uvs = new Vector2[vertices.Length];
+        uvs = mesh.uv;
+
+        for (int i = 0; i < uvs.Length; i++)
+        {
+            uvs[i] = new Vector2(uvs[i].x + offSetX, uvs[i].y);
+        }
+        mesh.uv = uvs;
+    }
+}
