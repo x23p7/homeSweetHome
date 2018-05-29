b96f1d895ea3826131e1b319d433298af330af92
Christopher <s0557859@htw-berlin.de>
2018-05-29 21:34:37 +0200

    reworked the portal cam movement script so it works properly

--- Assets/Common/Scripts/PortalCamMove.cs
+++ Assets/Common/Scripts/PortalCamMove.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PortalCamMove : MonoBehaviour {
+public class PortalCamMove : MonoBehaviour
+{
     Transform playerCam;
     public Transform portal;
+    public GameObject myLandingPoint;
     Transform targetPoint;
     public LoadPortalScene loadingScript;
     // Use this for initialization
@@ -15,17 +17,21 @@ public class PortalCamMove : MonoBehaviour {
     }
     private void OnEnable()
     {
-       targetPoint = loadingScript.targetPoint;
+        targetPoint = loadingScript.targetPoint;
     }
     // Update is called once per frame
-    void FixedUpdate() {
-        if (targetPoint != null) { 
-        Vector3 playerOffSetFromPortal = playerCam.position - portal.position;
-        transform.position = targetPoint.position + playerOffSetFromPortal + (portal.transform.position.y - GlobalGameStateManager.instance.player.transform.position.y + GlobalGameStateManager.instance.player.transform.localScale.y/2) * Vector3.up;
-        float angularDifference = Quaternion.Angle(targetPoint.parent.rotation, portal.parent.rotation) -180;
-        Quaternion rotationalDifference = Quaternion.AngleAxis(angularDifference, Vector3.up);
-        Vector3 newCamDir = rotationalDifference * playerCam.forward;
-        transform.rotation = Quaternion.LookRotation(newCamDir, Vector3.up);
+    void FixedUpdate()
+    {
+        if (targetPoint != null)
+        {
+            Vector3 playerOffSetFromPortal = playerCam.position - portal.position;
+            float localOffSetForward = Vector3.Dot(myLandingPoint.transform.forward, playerOffSetFromPortal);
+            float localOffSetRight = Vector3.Dot(myLandingPoint.transform.right, playerOffSetFromPortal);
+            transform.position = targetPoint.position - localOffSetForward * targetPoint.forward - localOffSetRight * targetPoint.right + (portal.transform.position.y - GlobalGameStateManager.instance.player.transform.position.y + GlobalGameStateManager.instance.player.transform.localScale.y / 2) * Vector3.up;
+            float angularDifference = Quaternion.Angle(targetPoint.parent.rotation, portal.parent.rotation) - 180;
+            Quaternion rotationalDifference = Quaternion.AngleAxis(angularDifference, Vector3.up);
+            Vector3 newCamDir = rotationalDifference * playerCam.forward;
+            transform.rotation = Quaternion.LookRotation(newCamDir, Vector3.up);
         }
     }
 }
